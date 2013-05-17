e4f968764c90aea2ac2b5920adb45cb2092f671d
NITHIN-PC <Nitin@Nithin-PC>
2013-05-17 11:45:29 +0530

    y0

---
+++ application/views/call_pag.php
@@ -0,0 +1,22 @@
+
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        
+        <form action="../models/call_page11.php" method="post">
+Name: <input type="text" name="username">
+Age: <input type="text" name="password">
+<input type="submit">
+</form>
+        ?>
+    </body>
+</html>
